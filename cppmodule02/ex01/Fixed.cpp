7020ad41c6ef18b42610e691679b8e2bcaaaf957
Ahmet Mert Kalaman <ahkalama@k1m23s03.42kocaeli.com.tr>
2024-09-05 12:56:29 +0300

    gitsilindi

---
+++ cppmodule02/ex01/Fixed.cpp
@@ -0,0 +1,48 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+    this->fixed = 0;
+    std::cout << "Default consturctor called\n";
+}
+Fixed::Fixed(const int fi)
+{
+    std::cout << "Int constructor called\n";
+	this->fixed = (fi * 256);
+}
+Fixed::Fixed(const float ff)
+{
+    std::cout << "Float constructor called\n";
+	this->fixed = roundf(ff * 256);
+}
+Fixed::Fixed(const Fixed& copy)
+{
+    std::cout << "Copy constructor called\n"; 
+        *this = copy;
+}
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called\n";
+}
+Fixed &Fixed::operator=(const Fixed &newFixed)
+{
+    std::cout << "Copy assignment operator called\n";
+    if(this != &newFixed)
+    {
+        this->fixed = newFixed.fixed;
+    }
+    return(*this);
+}
+float Fixed::toFloat( void ) const
+{
+    return ((float)this->fixed / 256);
+}
+int Fixed::toInt( void ) const
+{
+    return (this->fixed / 256);
+}
+std::ostream &operator<<(std::ostream &o, const Fixed& fixed)
+{
+	o << fixed.toFloat();
+	return (o);
+}
